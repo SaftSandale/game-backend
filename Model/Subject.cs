2f8b25ab088bd78648d867bef97de052d69c3330
Sebastian <dickmeis.sebastian@t-online.de>
2021-04-15 16:14:31 +0200

    Model anppasungen nachtrag

---
+++ Model/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearningGame.Backend.Enums;
+
+namespace LearningGame.Backend.Model
+{
+    class Subject
+    {
+        public string SubjectName { get; set; }
+        public Dictionary<Difficulty, List<Exercise>> Exercises { get; set; }
+
+        public void FillExercises(Difficulty difficulty)
+        {
+
+        }
+    }
+}
