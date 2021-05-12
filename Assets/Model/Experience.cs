d18f07e023cd3501e3747a08694dcfe991049e4e
schreckman <manuel.schreck.1999@web.de>
2021-05-12 11:16:37 +0200

    Hinzufügen von XP

---
+++ Assets/Model/Experience.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+
+namespace Assets.Model
+{
+    public class Experience
+    {
+
+        public Experience()
+        {
+            XP = 0;
+            Level = 1;
+        }
+        public int XP { get; private set; }
+        public int Level { get; private set; }
+
+        public void addXP(int xp)
+        {
+            this.XP += xp;
+
+            int neededXP = Level * 150;
+
+            if(this.XP > neededXP)
+            {
+                Level++;
+            }
+        }
+
+        
+    }
+}
