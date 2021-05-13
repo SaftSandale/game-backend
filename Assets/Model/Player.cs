2ae5751a587bcd7d175be90904bcb0db9bc50389
schreckman <manuel.schreck.1999@web.de>
2021-05-13 22:53:05 +0200

    Hinzufügen des Fragen Caches

--- Assets/Model/Player.cs
+++ Assets/Model/Player.cs
@@ -1,4 +1,5 @@
-﻿using PokAEmon.Model;
+﻿using PokAEmon.Enums;
+using PokAEmon.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +8,66 @@ using System.Threading.Tasks;
 
 namespace PokAEmon.Model
 {
+
     public class Player
     {
+        internal class QuestionCache
+        {
+            public int Index { get; private set; }
+
+            private int BonusLimit { get; set; }
+
+            public QuestionCache(int limit)
+            {
+                Index = 0;
+                BonusLimit = limit;
+            }
+
+            public void AddElement(bool question)
+            {
+                if (question == true)
+                {
+                    Index++;
+                }
+                else
+                {
+                    Index = 0;
+                }
+            }
+
+            public int CalcBonus()
+            {
+                return (Index % BonusLimit);
+            }
+        }
+
         public string PlayerName { get; set; }
         public Experience PlayerExperience { get; set; }
+        private QuestionCache QC { get; set; }
 
         public Player(string name, Experience xp)
         {
             PlayerName = name;
             PlayerExperience = xp;
+            QC = new QuestionCache(10);
         }
+
+        public void AddXP(Difficulty dif, bool answerStatus)
+        {
+            QC.AddElement(answerStatus);
+            if(QC.CalcBonus() == 0)
+            {
+                PlayerExperience.resetBonus();
+            }
+            else
+            {
+                PlayerExperience.setBonus((float)(0.05 * QC.CalcBonus()));
+            }
+            PlayerExperience.addXP(dif);
+            
+        }
+
+
 
     }
 }
